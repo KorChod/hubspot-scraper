3496b21a495543d4ddc739b536ce19e3f36f6a3f
KorChod <korneliuszchodnikiewicz@gmail.com>
2023-10-09 15:01:50 +0200

    Reformat

--- scraper.py
+++ scraper.py
@@ -6,8 +6,6 @@ from bs4 import BeautifulSoup
 from rake_nltk import Rake
 
 
-r = Rake()
-
 SOURCE_URL = 'https://blog.hubspot.com/'
 SOUP_PARSER = 'html.parser'
 DATE_PATTERN = r'\d{1,2}/\d{1,2}/\d{2}'
@@ -15,7 +13,6 @@ LIMIT_BLOG_POSTS = 3
 LIMIT_KEY_PHRASES = 5
 MAX_NUMBER_WORDS_IN_PHRASE = 3
 
-# Selector = namedtuple('Selector', 'primary')
 BlogPost = namedtuple('BlogPost', 'url date')
 
 
@@ -33,16 +30,57 @@ def get_soup(page_url):
     return BeautifulSoup(source.text, SOUP_PARSER)
 
 
-def process_text(blog_post):
+def get_blogposts(soup, selector):
+    blogpost_list = []
+    blogposts = soup.find_all('div', class_=selector)
+    for blogpost in blogposts:
+        secondary_selector = selector + '-title'
+        blogpost_url = blogpost.find(class_=secondary_selector).a.get('href')
+
+        blogpost_date = None
+        if selector == 'blog-post-card':
+            blogpost_date = blogpost \
+                .find(class_='blog-post-card-date') \
+                .get('datetime')
+        elif selector == 'blog-categories-card':
+            blogpost_date = blogpost \
+                .find(class_='blog-categories-card-footer') \
+                .find(string=re.compile(DATE_PATTERN)) \
+                .strip()
+
+        if blogpost_url and blogpost_date:
+            blogpost_list.append(BlogPost(blogpost_url, blogpost_date))
+
+    return blogpost_list
+
+
+def extract_data(blog_post):
     blog_post_soup = get_soup(blog_post.url)
     blog_post_text = blog_post_soup.find(class_='hsg-rich-text').get_text()
+
     word_count = len(re.findall(r'\b\w+\b', blog_post_text))
     letter_count = len(re.findall(r'[a-zA-Z]', blog_post_text))
+
+    r = Rake()
     r.extract_keywords_from_text(blog_post_text)
-    ranked_phrases = [phrase for phrase in r.get_ranked_phrases_with_scores() if len(phrase[1].split()) <= MAX_NUMBER_WORDS_IN_PHRASE]
+
+    words_condition = lambda x: len(x) <= MAX_NUMBER_WORDS_IN_PHRASE
+
+    ranked_phrases = r.get_ranked_phrases_with_scores()
+    ranked_phrases = [phrase for phrase in ranked_phrases if words_condition(phrase[1].split())]
     top_ranked_phrases = ranked_phrases[:LIMIT_KEY_PHRASES]
 
     return blog_post.url, word_count, letter_count, top_ranked_phrases
+
+
+def process_data(blog_posts):
+    num_processes = len(blog_posts)
+    pool = multiprocessing.Pool(processes=num_processes)
+
+    results = pool.map(extract_data, blog_posts)
+    pool.close()
+    pool.join()
+    return results
 
 
 def print_results(results):
@@ -54,37 +92,20 @@ def print_results(results):
         print(f"Key phrases: {ranked_phrases}", end='\n\n')
 
 
-soup = get_soup(SOURCE_URL)
+def main():
+    soup = get_soup(SOURCE_URL)
+
+    blogposts = []
+    blogposts += get_blogposts(soup, 'blog-post-card')
+    blogposts += get_blogposts(soup, 'blog-categories-card')
+
+    sorted_blogposts = sorted(blogposts, key=lambda x: (date_sort_key(x.date)), reverse=True)
+    top_sorted_blogposts = sorted_blogposts[:LIMIT_BLOG_POSTS]
+
+    results = process_data(top_sorted_blogposts)
+
+    print_results(results)
 
 
-# selectors = [Selector('blog-post-card'), Selector('blog-categories-card')]
-articles1 = soup.find_all('div', class_='blog-post-card')
-articles2 = soup.find_all('div', class_='blog-categories-card')
-
-blog_posts = []
-
-for article in articles1:
-    blog_post_url = article.find(class_='blog-post-card-title').a.get('href')
-    blog_post_date = article.find(class_='blog-post-card-date').get('datetime')
-    if blog_post_url and blog_post_date:
-        blog_posts.append(BlogPost(blog_post_url, blog_post_date))
-
-
-for article in articles2:
-    blog_post_url = article.find(class_='blog-categories-card-title').a.get('href')
-    blog_post_date = article.find(class_='blog-categories-card-footer').find(string=re.compile(DATE_PATTERN)).strip()
-    if blog_post_url and blog_post_date:
-        blog_posts.append(BlogPost(blog_post_url, blog_post_date))
-
-top_sorted_blog_posts = sorted(blog_posts, key= lambda x: (date_sort_key(x.date)), reverse=True)[:LIMIT_BLOG_POSTS]
-
-num_processes = len(top_sorted_blog_posts)
-pool = multiprocessing.Pool(processes=num_processes)
-
-results = pool.map(process_text, top_sorted_blog_posts)
-pool.close()
-pool.join()
-
-print_results(results)
-
-
+if __name__ == "__main__":
+    main()
