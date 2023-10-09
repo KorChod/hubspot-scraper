a1fb0a6d69c26879202fdf54f3f0d2b21cfe2da9
KorChod <korneliuszchodnikiewicz@gmail.com>
2023-10-09 14:08:50 +0200

    Reformat

--- scraper.py
+++ scraper.py
@@ -1,16 +1,22 @@
-from bs4 import BeautifulSoup
-import requests
-from collections import namedtuple
 import re
-# from keybert import KeyBERT
+import requests
+import multiprocessing
+from collections import namedtuple
+from bs4 import BeautifulSoup
 from rake_nltk import Rake
-r = Rake()
 
+
+r = Rake()
 
 SOURCE_URL = 'https://blog.hubspot.com/'
 SOUP_PARSER = 'html.parser'
 DATE_PATTERN = r'\d{1,2}/\d{1,2}/\d{2}'
 LIMIT_BLOG_POSTS = 3
+LIMIT_KEY_PHRASES = 5
+MAX_NUMBER_WORDS_IN_PHRASE = 3
+
+# Selector = namedtuple('Selector', 'primary')
+BlogPost = namedtuple('BlogPost', 'url date')
 
 
 def date_sort_key(date_str):
@@ -26,11 +32,32 @@ def get_soup(page_url):
         print(f"Failed to get the page content for '{page_url}': {e}")
     return BeautifulSoup(source.text, SOUP_PARSER)
 
+
+def process_text(blog_post):
+    blog_post_soup = get_soup(blog_post.url)
+    blog_post_text = blog_post_soup.find(class_='hsg-rich-text').get_text()
+    word_count = len(re.findall(r'\b\w+\b', blog_post_text))
+    letter_count = len(re.findall(r'[a-zA-Z]', blog_post_text))
+    r.extract_keywords_from_text(blog_post_text)
+    ranked_phrases = [phrase for phrase in r.get_ranked_phrases_with_scores() if len(phrase[1].split()) <= MAX_NUMBER_WORDS_IN_PHRASE]
+    top_ranked_phrases = ranked_phrases[:LIMIT_KEY_PHRASES]
+
+    return blog_post.url, word_count, letter_count, top_ranked_phrases
+
+
+def print_results(results):
+    for result in results:
+        blog_post_url, word_count, letter_count, ranked_phrases = result
+        print(f"Blogpost: {blog_post_url}")
+        print(f"Word count: {word_count}")
+        print(f"Letter count: {letter_count}")
+        print(f"Key phrases: {ranked_phrases}", end='\n\n')
+
+
 soup = get_soup(SOURCE_URL)
 
-Selector = namedtuple('Selector', 'primary')
-BlogPost = namedtuple('BlogPost', 'url date')
-selectors = [Selector('blog-post-card'), Selector('blog-categories-card')]
+
+# selectors = [Selector('blog-post-card'), Selector('blog-categories-card')]
 articles1 = soup.find_all('div', class_='blog-post-card')
 articles2 = soup.find_all('div', class_='blog-categories-card')
 
@@ -51,20 +78,13 @@ for article in articles2:
 
 top_sorted_blog_posts = sorted(blog_posts, key= lambda x: (date_sort_key(x.date)), reverse=True)[:LIMIT_BLOG_POSTS]
 
+num_processes = len(top_sorted_blog_posts)
+pool = multiprocessing.Pool(processes=num_processes)
 
-for blog_post in top_sorted_blog_posts:
-    blog_post_soup = get_soup(blog_post.url)
-    blog_post_text = blog_post_soup.find(class_='hsg-rich-text').get_text()
-    word_count = len(re.findall(r'\b\w+\b', blog_post_text))
-    letter_count = len(re.findall(r'[a-zA-Z]', blog_post_text))
-    # kw_model = KeyBERT()
-    # keywords = kw_model.extract_keywords(blog_post_text, keyphrase_ngram_range=(1, 3))
-    # print(keywords)
-    r.extract_keywords_from_text(blog_post_text)
-    for item in [keyword for keyword in r.get_ranked_phrases_with_scores() if len(keyword[1].split()) == 3]:
-        print(item)
+results = pool.map(process_text, top_sorted_blog_posts)
+pool.close()
+pool.join()
 
-
-
+print_results(results)
 
 
